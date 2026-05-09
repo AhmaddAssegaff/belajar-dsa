29b9fbf1d800ddfcd658cc6f0b2b10afd65b78b0
Ahmad <ahmadasgf89@gmail.com>
2026-05-09 20:48:33 +0700

    chore: add valid palindrome

---
+++ array_&_string/valid_palindrome.py
@@ -0,0 +1,25 @@
+# kata/kalimat yang dibaca dari depan dan belakang tetap sama
+# bandingkan karakter dari kiri dan kanan
+# jika semua sama maka palindrome
+# jika ada yang berbeda maka bukan palindrome
+# harus pake 2 pointer (left & right)
+
+s = "A man a plan a canal Panama madam"
+word = s.replace(" ", "").lower()
+
+is_palindrome = True
+
+left = 0
+right = len(word) - 1
+
+while left < right:
+    print(word[left], word[right])
+
+    left += 1
+    right -= 1
+
+    if word[left] != word[right]:
+        is_palindrome = False
+
+
+print(is_palindrome)
